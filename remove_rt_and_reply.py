2eb8915ddf691e3fbcc3a49d8301d4d1557776a2
yudejp <i@yude.jp>
2021-03-17 23:13:34 +0900

    Add "»" to exclude pattern

--- remove_rt_and_reply.py
+++ remove_rt_and_reply.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('tweets.csv')
-df = df[~df['full_text'].str.contains('RT|\@')]
+df = df[~df['full_text'].str.contains('RT|\@|»')]
 df.to_csv('tweets_processed.csv', encoding='utf_8_sig')
